98b43e87c5e9986a7ceef54c843ff9ce9f86c714
marlenewamalwa <wamalwamarlene@gmail.com>
2025-08-05 11:12:12 +0300

    first setup

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+// postcss.config.cjs
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    // other plugins as needed
+  ],
+}
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-500">
+      Health Shots App
+    </div>
+  );
+}
+
+export default App;
